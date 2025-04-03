b10f95e31e1c7d5f5fa70def006aae38e14cfa8d
myduyen3103 <vtmyduyen3103@gmail.com>
2025-04-03 09:32:54 +0700

    3rd commit

--- src/main/java/controller/AccountManagement.java
+++ src/main/java/controller/AccountManagement.java
@@ -1,19 +1,4 @@
-package controller;
 
-import jobtrans.dal.AccountDAO;
-import jobtrans.model.Account;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet(name="AccountManagement", urlPatterns={"/acc-manage"})
 public class AccountManagement extends HttpServlet {
